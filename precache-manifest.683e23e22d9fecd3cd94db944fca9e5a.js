cdf25adbf043cf5093056759af995cf7aca75a74
Akhlaque <akhlaquea01@gmail.com>
2021-01-19 14:44:57 +0530

    Updates

---
+++ precache-manifest.683e23e22d9fecd3cd94db944fca9e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645bb9ae67e264ca4c0c78290281aafe",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "bead3c6b8b0d2bbf9d18",
+    "url": "/To-Do-App-in-React/static/css/main.f171ecbb.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/To-Do-App-in-React/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "bead3c6b8b0d2bbf9d18",
+    "url": "/To-Do-App-in-React/static/js/main.07247026.chunk.js"
+  },
+  {
+    "revision": "759cf7b4cf17cf63d488",
+    "url": "/To-Do-App-in-React/static/js/runtime~main.60884708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
